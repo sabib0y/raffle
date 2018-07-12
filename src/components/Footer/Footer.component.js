5b4c425f01fc689ed7ed2a43cd20c616da694d55
DESKTOP-PHNFC0T\i_am9 <leslie.khumalo@icloud.com>
2018-07-12 20:50:55 +0100

    updated footer

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -4,7 +4,59 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className="socialLinks">
-      PUT SOCIAL LINKS HERE
+      test
+      <ul center>
+        <li>
+          <a href="#" title="GitHub">
+            <i className="fa fa-github"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="CodePen">
+            <i className="fa fa-codepen"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Dribbble">
+            <i className="fa fa-dribbble"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="StackOverflow">
+            <i className="fa fa-stack-overflow"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="LinkedIn">
+            <i className="fa fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Twitter">
+            <i className="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Instagram">
+            <i className="fa fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Medium">
+            <i className="fa fa-medium"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Resume">
+            <i className="fa fa-file-pdf-o"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Contact">
+            <i className="fa fa-send"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 };
