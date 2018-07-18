2bcfa43d9a9bff437a91c521d881660c1b8e1b13
DESKTOP-PHNFC0T\i_am9 <leslie.khumalo@icloud.com>
2018-07-18 21:43:34 +0100

    updated code

--- src/App.js
+++ src/App.js
@@ -110,11 +110,13 @@ class App extends Component {
         {App.isIntervalPreForm(newArray[0], newArray[3], newArray[4], nowTime) &&
           <InterimPage
             textInterim='New competition entry will be available shortly ... Thank you.'
+            schedule="form"
           />
         }
          {App.isIntervalPreResults(newArray[1], newArray[2], nowTime) &&
           <InterimPage
             textInterim='Results will be published shortly... Thank you.'
+            schedule="results"
           />
         }
         <Footer />
--- src/components/InterimPage/InterimPage.js
+++ src/components/InterimPage/InterimPage.js
@@ -1,31 +1,68 @@
 import React from 'react';
 import './InterimPage.scss';
 import fire from '../../fire';
-import timerInterval from '../../helpers/timerinterval';
+import moment from 'moment-timezone';
+import Countdown from 'react-countdown-now';
 
 export default class InterimPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      formStartToCheck: null,
+      resultStartToCheck: null
+    }
+  }
 
-  handleTimeInterval(){
-    let data = [];
-    let sortedData = [];
+  componentDidMount(){
+    let collectedData;
     fire.database().ref('setTimeForm/postData').once('value').then((snapshot) => {
-      let collectedData = Object.entries(snapshot.val());
-      collectedData.map(item => {
-        data.push(item);
-      });
-      for (let value of data) {
-        console.log(value, 'value');
+      collectedData = snapshot.val();
+
+      let resultStartTime = new Date(collectedData.resultStart);
+      let formStartTime = new Date(collectedData.formStart);
+      let timeNow = new Date();
+
+      let timeMathResultStartTime = resultStartTime - timeNow;
+      let timeMathFormStartTime;
+
+      if(timeNow > formStartTime){
+        let day;
+        day = moment(formStartTime).add(1, 'd');
+        console.log('day', day.format());
+        timeMathFormStartTime = day - timeNow;
+        this.setState({
+          formStartToCheck: timeMathFormStartTime,
+        })
       }
+      else {
+        timeMathFormStartTime = formStartTime - timeNow;
+        this.setState({
+          formStartToCheck: timeMathFormStartTime,
+        })
+      }
+      this.setState({
+        resultStartToCheck: timeMathResultStartTime
+      });
     });
-    console.log('test', data)
   };
 
   render () {
-    this.handleTimeInterval();
+    const { formStartToCheck, resultStartToCheck } =this.state;
+
     return (
       <div className="containerWrapper">
         <p>Competition is currently closed...</p>
         <p>{this.props.textInterim}</p>
+        {this.props.schedule === 'form' &&
+          <Countdown date={Date.now() + formStartToCheck}>
+            <span>You are good to go FORM</span>
+          </Countdown>
+        }
+        {this.props.schedule === 'results' &&
+          <Countdown date={Date.now() + resultStartToCheck}>
+            <span>You are good to go Results</span>
+          </Countdown>
+        }
       </div>
     )
   }
