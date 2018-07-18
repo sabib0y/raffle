deeddcc0ad32426b738cf2bc28490eeb115b3d15
DESKTOP-PHNFC0T\i_am9 <leslie.khumalo@icloud.com>
2018-07-18 18:50:44 +0100

    updated time interval

--- src/components/InterimPage/InterimPage.js
+++ src/components/InterimPage/InterimPage.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './InterimPage.scss';
+import fire from '../../fire';
+import timerInterval from '../../helpers/timerinterval';
 
 export default class InterimPage extends React.Component {
+
+  handleTimeInterval(){
+    let data = [];
+    let sortedData = [];
+    fire.database().ref('setTimeForm/postData').once('value').then((snapshot) => {
+      let collectedData = Object.entries(snapshot.val());
+      collectedData.map(item => {
+        data.push(item);
+      });
+      for (let value of data) {
+        console.log(value, 'value');
+      }
+    });
+    console.log('test', data)
+  };
+
   render () {
+    this.handleTimeInterval();
     return (
       <div className="containerWrapper">
         <p>Competition is currently closed...</p>
--- src/helpers/getDataFirebase.js
+++ src/helpers/getDataFirebase.js
@@ -4,7 +4,7 @@ export default function getPosts(item) {
   let collectedData = [];
   let randomizedData = [];
   fire.database().ref(item).once('value').then((snapshot) => {
-    let receivedDataTest = snapshot.val();
+    // let receivedDataTest = snapshot.val();
     let receivedData = Object.entries(snapshot.val());
     receivedData.map(item => {
       collectedData.push(item[1].user)
