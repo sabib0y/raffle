a6185dcf8a8a89a2eeab4f45afe2fda0b7fbd2e7
paul idemudia <osaspofficial@gmail.com>
2018-07-18 22:38:27 +0100

    paul f up

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -28,30 +28,31 @@ const Footer = () => {
     <div className="footerLinkWrapper">
       <ul>
         <li>
-          <a href="#" title="Facebook">
+          <a href="https://www.facebook.com/daily.choppins" target="_blank" title="Facebook">
             <FontAwesomeIcon icon={['fab', 'facebook-f']} />
           </a>
         </li>
         <li>
-          <a href="#" title="Twitter">
+          <a href="https://www.instagram.com/dailychoppinss" target="_blank" title="Twitter">
+          {/* to be added in */}
             <FontAwesomeIcon icon={['fab', 'twitter']} />
           </a>
         </li>
         <li>
-          <a href="#" title="Instagram">
+          <a href="https://www.instagram.com/dailychoppinss" target="_blank" title="Instagram">
             <FontAwesomeIcon icon={['fab', 'instagram']} />
           </a>
         </li>
         <li>
-          <a href="#" title="Telegram">
+          <a href="https://t.me/daaterror" target="_blank" title="Telegram">
             <FontAwesomeIcon icon={['fab', 'telegram']} />
           </a>
         </li>
-        <li>
-          <a href="#" title="Envelope">
+        {/* <li>
+          <a href="#" target="_blank" title="Envelope">
             <FontAwesomeIcon icon={faEnvelope} />
           </a>
-        </li>
+        </li> */}
       </ul>
       <p className="disclaimerText">By entering the competition, you consent to your details being used for marketing purposes.</p>
     </div>
