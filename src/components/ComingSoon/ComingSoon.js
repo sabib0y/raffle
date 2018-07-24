61f836eb0197719be17feec55616b2303ec56d49
DESKTOP-PHNFC0T\i_am9 <leslie.khumalo@icloud.com>
2018-07-24 22:57:57 +0100

    updated timer

--- src/components/ComingSoon/ComingSoon.js
+++ src/components/ComingSoon/ComingSoon.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../Footer/Footer.component';
 import { connect } from "react-redux";
 import Countdown from 'react-countdown-now';
+import moment from 'moment-timezone';
 import fire from '../../fire';
 import './ComingSoon.scss';
 
@@ -9,7 +10,12 @@ export class ComingSoon extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeMathResultStartTime: ''
+      timeMathResultStartTime: '',
+      newCountDown: '',
+      day: '',
+      hour: '',
+      min: '',
+      sec: ''
     };
     this.renderer = this.renderer.bind(this);
   }
@@ -37,6 +43,7 @@ export class ComingSoon extends React.Component {
 
   componentDidMount() {
     let collectedData;
+    let dateToTestAgainst;
     fire.database().ref('setSiteLaunch/siteLaunch').once('value').then((snapshot) => {
       collectedData = snapshot.val();
       console.log('collectedData', collectedData);
@@ -48,7 +55,47 @@ export class ComingSoon extends React.Component {
       this.setState({
         timeMathResultStartTime
       });
-      console.log('ttttttt', this.state.timeMathResultStartTime);
+
+      dateToTestAgainst = new  Date(collectedData);
+      let eventTime= 1366549200; // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
+      let currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+      let diffTime = dateToTestAgainst - timeNow;
+      let duration = moment.duration(diffTime*1000, 'milliseconds');
+      let interval = 1000;
+
+      setInterval(() => {
+        duration = moment.duration(duration - interval, 'milliseconds');
+        let testData = duration.days() + ":" + duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
+
+
+
+        console.log('testing this badboy', testData);
+
+        let day,
+          hour,
+          min,
+          sec;
+
+        if(testData.length > 1) {
+          let newArray = [];
+          newArray.push(testData.split(':'));
+
+          day = newArray[0][0];
+          hour = newArray[0][1];
+          min = newArray[0][2];
+          sec = newArray[0][3];
+
+          this.setState({
+            day,
+            hour,
+            min,
+            sec
+          });
+          // console.log('time converted:',newArray, day, hour, min, sec);
+        }
+
+        // $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
+      }, interval);
     });
   }
 
@@ -60,11 +107,28 @@ export class ComingSoon extends React.Component {
           <h3 className="headerText">coming soon</h3>
         </div>
         <div className="centreText">
-          <Countdown
-            date={Date.now() + timeMathResultStartTime}
-            onComplete={this.renderer}
-          >
-          </Countdown>
+          <div className="timeCountDown">{this.state.newCountDown}</div>
+          {/*<Countdown*/}
+            {/*date={Date.now() + timeMathResultStartTime}*/}
+            {/*onComplete={this.renderer}*/}
+          {/*>*/}
+          {/*</Countdown>*/}
+          <span>
+            <span>Days</span>
+            <span>{this.state.day}</span>
+          </span>
+          <span>
+            <span>Hours</span>
+            <span>{this.state.hour}</span>
+          </span>
+          <span>
+            <span>Minutes</span>
+            <span>{this.state.min}</span>
+          </span>
+          <span>
+            <span>Seconds</span>
+            <span>{this.state.sec}</span>
+          </span>
         </div>
         <Footer/>
       </div>
