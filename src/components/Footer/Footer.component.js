f7fbc845849f97457d63c4082587f3a1423784f9
DESKTOP-PHNFC0T\i_am9 <leslie.khumalo@icloud.com>
2018-07-30 19:53:42 +0100

    updated links

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Footer.scss';
-import { Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import {
@@ -28,25 +27,25 @@ const Footer = () => {
     <div className="footerLinkWrapper">
       <ul>
         <li>
-          <Link to="https://www.facebook.com/DailyChoppins-461094991028290/?modal=admin_todo_tour" target="_blank" title="Facebook">
+          <a href="https://www.facebook.com/DailyChoppins-461094991028290/?modal=admin_todo_tour" target="_blank" title="Facebook">
             <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to="https://www.instagram.com/dailychoppinss" target="_blank" title="Twitter">
+          <a href="https://www.instagram.com/dailychoppinss" target="_blank" title="Twitter">
           {/* to be added in */}
             <FontAwesomeIcon icon={['fab', 'twitter']} />
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to="https://www.instagram.com/dailychoppinss" target="_blank" title="Instagram">
+          <a href="https://www.instagram.com/dailychoppinss" target="_blank" title="Instagram">
             <FontAwesomeIcon icon={['fab', 'instagram']} />
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to="https://t.me/dailychoppins" target="_blank" title="Telegram">
+          <a href="https://t.me/dailychoppins" target="_blank" title="Telegram">
             <FontAwesomeIcon icon={['fab', 'telegram']} />
-          </Link>
+          </a>
         </li>
       </ul>
     </div>
